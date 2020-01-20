a95d1874db0c5d06ea32f21f571cb9bb5e965813
gutovesco <guto.vesco@hotmail.com>
2020-01-20 18:59:17 -0300

    api stuff

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const APP_ID = "6118bec3";
+  const APP_KEY = "2aa007552e6b68a60c0631cf974326b1";
+
+  useEffect(() =>{
+    
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Happy Programming ;)
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="search-form">
+        <input className="search-bar" type="text"></input>
+        <button className="search-button" type="submit">Search</button>
+      </form>
     </div>
-  );
+  )
 }
 
 export default App;
